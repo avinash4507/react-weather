88a7ac572d2edfa4fc66c5bb390e1b042542c1b2
avinash4507 <avinash4507@gmail.com>
2017-03-18 13:54:21 +0530

    Foundation error fixes

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,27 +1,34 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
     getDefaultProps: function () {
-        return {
-            title: 'Error'
-        };
+        return {title: 'Error'};
     },
     propTypes: {
         title: React.PropTypes.string,
         message: React.PropTypes.string.isRequired
     },
     componentDidMount: function () {
-        var modal = new Foundation.Reveal(jQuery('#error-modal'));
-        modal.open();
-    },
-    render: function () {
-        var { title, message } = this.props;
-        return (
+        var {title, message} = this.props;
+        var modalMarkup = (
             <div className="reveal tiny text-center" id="error-modal" data-reveal="">
                 <h1>{title}</h1>
                 <p>{message}</p>
                 <button className="button hollow" data-close="">Okay</button>
             </div>
+        );
+
+        let $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
+        var modal = new Foundation.Reveal(jQuery('#error-modal'));
+        modal.open();
+    },
+    render: function () {
+        return (
+            <div></div>
         );
     }
 });
